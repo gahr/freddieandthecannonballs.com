261a7f1c2cd33cf3bad33e91c32c9c76354868e7
Pietro Cerutti <gahr@gahr.ch>
2019-05-09 11:13:59 +0000

    Filter future events run-time

--- data/scripts.js
+++ data/scripts.js
@@ -5,3 +5,16 @@ const setBio = function() {
   setter('en');
   return setter;
 }();
+
+/* Filter events so that only future ones are displayed */
+(function() {
+  const now = new Date().toJSON().slice(0, 10);
+  $('.event-row').each(function(index, value) {
+    const eventNode = value.cells.item(0);
+    if (eventNode.textContent < now) {
+      $(this).addClass("d-none");
+    }
+  });
+}());
+
+/* vim: set ts=2 sw=2 expandtab: */
