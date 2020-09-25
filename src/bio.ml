757362969979217a34015e0d57d8f0bd45ba2412
gahr <gahr>
2020-09-25 12:23:17 +0000

    Better parameter name

--- src/bio.ml
+++ src/bio.ml
@@ -2,4 +2,4 @@ open Core_kernel
 
 let fetch ~lang ~handler =
   let url = "./data/bio-" ^ (Lang.short_string_of_lang lang) ^ ".txt" in
-  Url.fetch ~url ~not_found_msg:"Cannot fetch bio" ~handler
+  Url.fetch ~url ~on_error_msg:"Cannot fetch bio" ~handler
--- src/url.ml
+++ src/url.ml
@@ -1,8 +1,8 @@
-let fetch ~url ~not_found_msg ~handler =
+let fetch ~url ~on_error_msg ~handler =
   let open Async_kernel in
   let handle = function
     | Result.Ok body -> handler body
-    | Result.Error _ -> handler not_found_msg
+    | Result.Error _ -> handler on_error_msg
   in
   Async_js.Http.get url >>| handle >>> fun _ -> ()
 
