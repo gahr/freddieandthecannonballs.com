84352cd861f766a8fb76c369142edd1cb2ef4cce
Pietro Cerutti <gahr@gahr.ch>
2020-09-29 10:08:40 +0000

    Use Core.List functions

--- src/parse.ml
+++ src/parse.ml
@@ -1,31 +1,6 @@
 open Incr_dom.Vdom
 open Base
 
-let take i xs =
-  let rec aux acc i xs =
-    match i with
-    | 0 -> acc
-    | k -> match xs with
-      | [] -> []
-      | y :: ys -> aux (y :: acc) (k - 1) ys
-  in
-  aux [] i xs |> List.rev
-
-let rec drop i xs =
-  match i with
-  | 0 -> xs
-  | k -> match xs with
-    | [] -> []
-    | _::ys -> drop (k - 1) ys
-
-let index_of elt xs =
-  let rec aux i elt = function
-    | [] -> None
-    | x :: _ when Char.(x = elt) -> Some i
-    | _ :: xs -> aux (i + 1) elt xs
-  in
-  aux 0 elt xs
-
 let rec parse (result : Node.t list) text_so_far chars =
   let handle_char c xs =
     parse result (text_so_far ^ (String.of_char c)) xs
@@ -37,14 +12,17 @@ let rec parse (result : Node.t list) text_so_far chars =
   in
 
   let handle_open_brace xs =
-    match index_of '}' xs with
+    let is_open _ x = Char.(x = '{') in
+    let is_close _ x = Char.(x = '}') in
+
+    match List.findi xs ~f:is_close with
     | None -> handle_char '{' xs
-    | Some idx_close ->
-      match index_of '{' xs with
-      | Some idx_open when idx_open < idx_close -> handle_char '{' xs
+    | Some (idx_close, _) ->
+      match List.findi xs ~f:is_open with
+      | Some (idx_open, _) when idx_open < idx_close -> handle_char '{' xs
       | _ -> handle_bold
-               (take idx_close xs |> String.of_char_list)
-               (drop (idx_close + 1) xs)
+               (List.take xs idx_close |> String.of_char_list)
+               (List.drop xs (idx_close + 1))
   in
 
   match chars with
