e91783891144e0713f8fe6621d8fd8e6c7dc8e8f
Pietro Cerutti <gahr@gahr.ch>
2020-09-29 09:05:53 +0000

    Use don't_wait_for

--- src/url.ml
+++ src/url.ml
@@ -4,5 +4,4 @@ let fetch ~url ~on_error_msg ~handler =
     | Result.Ok body -> handler body
     | Result.Error _ -> handler on_error_msg
   in
-  Async_js.Http.get url >>| handle >>> fun _ -> ()
-
+  don't_wait_for (Async_js.Http.get url >>| handle)
