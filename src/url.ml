a69f128865bf2a3539d4f00ff67fc60c11882b1d
gahr <gahr>
2020-09-29 09:17:12 +0000

    Local open

--- src/url.ml
+++ src/url.ml
@@ -1,7 +1,6 @@
 let fetch ~url ~on_error_msg ~handler =
-  let open Async_kernel in
   let handle = function
     | Result.Ok body -> handler body
     | Result.Error _ -> handler on_error_msg
   in
-  don't_wait_for (Async_js.Http.get url >>| handle)
+  Async_kernel.(don't_wait_for (Async_js.Http.get url >>| handle))
