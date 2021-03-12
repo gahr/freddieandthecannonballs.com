23e060de6cc9b3dfc26a7f4c9a190a35f9623be9
gahr <gahr>
2021-03-12 11:59:45 +0000

    Make everything 2 spaces

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-  function translate(lang) {
+  ["en", "it"].forEach(lang => {
     $("#lang-".concat(lang)).click(function() {
       $.getJSON("?action=get-i18n&lang=".concat(lang), function(data) {
-          for (var key in data)
-          {
-            $("#i18n-".concat(key)).html(data[key]);
-          }
+        for (var key in data)
+        {
+          $("#i18n-".concat(key)).html(data[key]);
+        }
       });
     });
-  }
-  ["en", "it"].forEach(lang => {
-    translate(lang);
   });
 });
+
+// vim: set ts=2 sw=2 expandtab:
