b5552ce1d7af395607febadf833e9aa69f952a66
gahr <gahr>
2024-10-19 13:35:00 +0000

    Log data sync

--- data.scm
+++ data.scm
@@ -2,19 +2,20 @@
 
   (import
     (scheme)
-    (chicken base)
-    (chicken format)
-    (chicken port)
-    (chicken process)
     (openssl)
-    (http-client)
-    (only (spiffy) root-path))
+    (only (chicken format) sprintf)
+    (only (chicken port) copy-port)
+    (only (chicken process) call-with-output-pipe)
+    (only (chicken time) current-seconds)
+    (only (chicken time posix) seconds->string)
+    (only (http-client) call-with-input-request)
+    (only (spiffy) access-log log-to root-path))
 
   (define (unzip in)
     (let ((s (sprintf "/usr/bin/unzip -qud ~A/data -- -" (root-path))))
-      (printf "cmd: ~A" s)
       (call-with-output-pipe s (lambda (out) (copy-port in out)))))
 
   (define (get url)
+    (log-to (access-log) "[~A] data sync'd" (seconds->string (current-seconds)))
     (call-with-input-request url #f unzip))
 )
